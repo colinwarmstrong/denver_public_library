40d54f295f6a7a9e58cfcca21343ca90a641ab36
Colin Armstrong <colinwarmstrong@gmail.com>
2018-05-24 09:28:56 -0600

    Creates attributes for author class and methods to read them.

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,10 @@
 class Author
+  attr_reader :first_name, :last_name, :books
+
+  def initialize(name)
+    @first_name = name[:first_name]
+    @last_name = name[:last_name]
+    @books = []
+  end
 
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -4,9 +4,17 @@ require './lib/author.rb'
 class AuthorTest < Minitest::Test
 
   def test_authors_exits
-    author = Author.new
+    author = Author.new({first_name: "Charlotte", last_name: "Bronte"})
 
     assert_instance_of Author, author
+  end
+
+  def test_authors_are_initialized_with_names_and_an_empty_book_array
+    author = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    assert_equal 'Charlotte', author.first_name
+    assert_equal 'Bronte', author.last_name
+    assert_equal [], author.books
   end
 
 end
