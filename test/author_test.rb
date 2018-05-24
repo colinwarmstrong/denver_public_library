3df7bfa531e5bc1142f979bf79b5c6ee41ca5dfe
Colin Armstrong <colinwarmstrong@gmail.com>
2018-05-24 09:24:25 -0600

    Adds initial test for the author class.

--- test/author_test.rb
+++ test/author_test.rb
@@ -1,6 +1,12 @@
 require './test/test_helper'
+require './lib/author.rb'
 
 class AuthorTest < Minitest::Test
 
+  def test_authors_exits
+    author = Author.new
+
+    assert_instance_of Author, author
+  end
 
 end
