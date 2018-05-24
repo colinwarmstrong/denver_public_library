5e5569dab012c494456c1b87d5ce5c9f73866fa3
Colin Armstrong <colinwarmstrong@gmail.com>
2018-05-24 09:21:15 -0600

    Adds attributes to the book class and methods to read these attributes.

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,18 @@
 class Book
+  attr_reader :author_first_name,
+              :author_last_name,
+              :title,
+              :publication_date_full
 
+  def initialize(book_info)
+    @author_first_name = book_info[:author_first_name]
+    @author_last_name = book_info[:author_last_name]
+    @title = book_info[:title]
+    @publication_date_full = book_info[:publication_date]
+  end
+
+  def publication_date
+    @publication_date_full[-4..-1]
+  end
 
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -4,11 +4,18 @@ require './lib/book.rb'
 class BookTest < Minitest::Test
 
   def test_book_exists
-    book = Book.new
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
 
     assert_instance_of Book, book
   end
 
+  def test_book_can_be_assigned_basic_info_when_initialized
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
 
+    assert_equal 'Harper', book.author_first_name
+    assert_equal 'Lee', book.author_last_name
+    assert_equal 'To Kill a Mockingbird', book.title
+    assert_equal '1960', book.publication_date
+  end
 
 end
