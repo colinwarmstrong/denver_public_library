0e162b5e34df253fb3035fa5406b40a32b2d2a61
Colin Armstrong <colinwarmstrong@gmail.com>
2018-05-24 09:50:33 -0600

    Implements slight change to author tests.

--- test/author_test.rb
+++ test/author_test.rb
@@ -18,9 +18,14 @@ class AuthorTest < Minitest::Test
     assert_equal [], charlotte_bronte.books
   end
 
-  def test_we_can_add_books_to_an_author
+  def test_we_can_add_a_books_to_an_author
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
 
+    assert_equal 0, charlotte_bronte.books.length
     assert_instance_of Book, charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+    assert_equal 1, charlotte_bronte.books.length
+    assert_instance_of Book, charlotte_bronte.add_book("Villette", "1853")
+    assert_equal 2, charlotte_bronte.books.length
   end
+
 end
