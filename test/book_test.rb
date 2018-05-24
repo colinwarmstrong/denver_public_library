86fc3f65dbdb7e7ec30d70780ddbbe43a486d8e0
Colin Armstrong <colinwarmstrong@gmail.com>
2018-05-24 09:11:06 -0600

    Initial commit; adds basic framework from the book class.

--- test/book_test.rb
+++ test/book_test.rb
@@ -1,6 +1,14 @@
 require './test/test_helper'
+require './lib/book.rb'
 
 class BookTest < Minitest::Test
+
+  def test_book_exists
+    book = Book.new
+
+    assert_instance_of Book, book
+  end
+
 
 
 end
